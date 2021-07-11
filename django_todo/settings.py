d8311757a31a5411e40f6233c6dc731ea84a4e59
svickery <s.vickery@hotmail.co.uk>
2021-07-11 14:10:15 +0000

    Fix allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rcv3%32cjptkqc+o&4b&1zdl-*m3=jv3e1!6f=)8*$!33ka0u!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['svick-todo-app.herokuapp.com']
 
 
 # Application definition
